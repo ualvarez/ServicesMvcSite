03136065d29507e00cc238024f14536cf43b1b00
ualvarez <ualvarez95@gmail.com>
2020-04-20 22:08:58 -0300

    Until Video Tutorial number 40

---
+++ ServicesMvcSite/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1, Name="Mary", Department=Dept.HR, Email="mary@gmail.com"},
+                new Employee(){Id=2, Name="John", Department=Dept.IT, Email="john@gmail.com"},
+                new Employee(){Id=3, Name="Sam", Department=Dept.IT, Email="sam@gmail.com"}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
--- ServicesMvcSite/EmployeeManagement/Startup.cs
+++ ServicesMvcSite/EmployeeManagement/Startup.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,22 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews().AddXmlSerializerFormatters();            
+            //services.AddMvc(option => option.EnableEndpointRouting = false).AddXmlSerializerFormatters();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+            services.AddMvc().AddRazorRuntimeCompilation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +40,26 @@ namespace EmployeeManagement
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+
+
+            //app.UseMvcWithDefaultRoute();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+               
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
+
+
         }
+
+
     }
 }
